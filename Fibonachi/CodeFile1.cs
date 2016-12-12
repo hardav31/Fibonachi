9890d5946d11d8c983ecf37c5a74727b556ad856
David <hardav31@mail.ru>
2016-12-12 21:20:19 +0400

    Add project files.

---
+++ Fibonachi/CodeFile1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+static class StaticClass
+{
+    public static void Method(int fib)
+    {
+
+        bool bo = true;
+        int x1 = 0;
+        int x2 = 1;
+        int x3 = 1;
+
+        if (fib < 0) { return; }
+        if (fib == 0) { Console.WriteLine(x1); }
+        if (fib == 1) { Console.WriteLine(x2); }
+        int i = 2;
+        while (bo)
+        {
+            x3 = x1 + x2;
+            if (i == fib)
+            {
+                bo = false;
+            }
+            i++;
+            x1 = x2;
+            x2 = x3;
+        }
+        Console.WriteLine(x3);
+    }
+}
+class davo 
+{
+    static void Main() 
+    {
+        StaticClass.Method(8);
+    }
+}
